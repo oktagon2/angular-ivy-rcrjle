8b1df7a47f205a9c994050a871ae2344b93a79d0
oktagon2 <ruschwa17@gmail.com>
2022-12-14 16:57:22 +0100

    2 files modified, 1 added

--- src/app/mat-select-search/mat-select-search.component.ts
+++ src/app/mat-select-search/mat-select-search.component.ts
@@ -1,32 +1,71 @@
-import {
-  Component,
-  OnInit,
-  OnChanges,
-  Input,
-  SimpleChanges,
-} from '@angular/core';
-
-import { Observable, of } from 'rxjs';
-import { FormControl } from '@angular/forms';
+import {Component, EventEmitter, Injector, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {FormControl, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {FormControlValueAccessorConnector} from "../utils/form-control-value-accessor-connector.component";
+import {debounceTime, Observable, of, startWith} from "rxjs";
 
 @Component({
   selector: 'app-mat-select-search',
   templateUrl: './mat-select-search.component.html',
   styleUrls: ['./mat-select-search.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: MatSelectSearchComponent,
+      multi: true
+    }
+  ]
 })
-export class MatSelectSearchComponent implements OnInit, OnChanges {
+export class MatSelectSearchComponent extends FormControlValueAccessorConnector implements OnInit, OnChanges {
   @Input() appearance: 'fill' | 'outline' = 'fill';
-  @Input() placeholder: string = 'Select X';
+  @Input() placeholder: string = "Specify placeholder='you placeholder here'";
   @Input() items: Observable<Array<any>> = of([]);
-  @Input() bindValueKey: string = 'value';
-  @Input() bindLabelKey: string = 'label';
-  @Input() searchPlaceholder: string = 'Search your item ...';
 
-  filterFormControl: FormControl = new FormControl('');
+  @Input() bindValueKey: string = "value";
+  @Input() bindLabelKey: string = "label";
+  @Input() searchPlaceholder: string = "Search your item ...";
+  @Output() itemFilterServerSide = new EventEmitter<string>();
+  filterFormControl: FormControl = new FormControl('')
+
+
   private isServerSide: boolean = true;
   private currentStaticItems: Array<any> = [];
 
-  ngOnInit(): void {}
+  constructor(injector: Injector) {
+    super(injector);
+  }
+
+  private listenToFilterFormControlChanges(): void {
+    this.filterFormControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+    ).subscribe((value: string) => {
+      if (this.isServerSide) {
+        this.itemFilterServerSide.emit(value);
+      } else {
+        this.filterStaticList(value);
+      }
+    });
+  }
+
+  private filterStaticList(value: string) {
+    const currentItems = this.currentStaticItems;
+
+    const filterValue = this._normalizeValue(value);
+    this.items = of(
+      currentItems.filter(item => this._normalizeValue(item).includes(filterValue))
+    );
+  }
+
+  private _normalizeValue(value: any): string {
+    if (typeof value != "string") {
+      value = value[this.bindLabelKey];
+    }
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+
+  ngOnInit(): void {
+    this.listenToFilterFormControlChanges();
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.items instanceof Array) {
@@ -35,4 +74,6 @@ export class MatSelectSearchComponent implements OnInit, OnChanges {
       this.isServerSide = false;
     }
   }
+
+
 }
